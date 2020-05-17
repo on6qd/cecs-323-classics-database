8b6545ed21e450b748c8e7659c6604edd953e5dc
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-16 17:53:55 -0700

    added question 4

--- queries.sql
+++ queries.sql
@@ -20,3 +20,6 @@ select customername from customers order by customername;
 
 -- 3) list each of the different status that an order may be in 
 SELECT distinct STATUS from ORDERS;
+
+-- 4) list firstname and lastname for each employee. sort by lastname then firstname
+select firstname, lastname from employees order by lastname, firstname;
