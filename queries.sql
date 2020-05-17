9a7237b38042aede404d866a7e9994f45d371afb
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-16 17:38:37 -0700

    updated queries.sql

---
+++ queries.sql
@@ -0,0 +1,16 @@
+-- 1) List all the data in the classics models database
+select * from PRODUCTLINES;
+
+select * from PRODUCTS;
+
+SELECT * FROM EMPLOYEES;
+
+SELECT * from OFFICES;
+
+SELECT * FROM CUSTOMERS;
+
+SELECT * FROM ORDERS;
+
+SELECT * FROM ORDERDETAILS;
+
+SELECT * FROM PAYMENTS;
